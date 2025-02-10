696cc13bad152aa199ed8733a15e9e99a8f36cff
lvy <upwardsrwr@gmail.com>
2025-02-10 22:40:04 +0800

    feat: add layout componet

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "antd";
+const Header = () => {
+  return (
+    <Layout.Header className="!bg-white border-b h-16 border-gray-300">
+      Header
+    </Layout.Header>
+  );
+};
+
+export default Header;
---
+++ src/components/Operate/index.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "antd";
+const Option = () => {
+  return (
+    <Layout.Sider className="!bg-white border-r border-gray-300">
+      Header
+    </Layout.Sider>
+  );
+};
+
+export default Option;
